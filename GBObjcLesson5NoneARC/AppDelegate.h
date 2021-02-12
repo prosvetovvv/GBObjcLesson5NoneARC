83dd098707169970b3604630f9aca3139e14fe84
prosvetovvv <vp12@yandex.ru>
2021-02-12 12:16:58 +0300

    Initial Commit

---
+++ GBObjcLesson5NoneARC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  GBObjcLesson5NoneARC
+//
+//  Created by Vitaly Prosvetov on 12.02.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ GBObjcLesson5NoneARC/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  GBObjcLesson5NoneARC
+//
+//  Created by Vitaly Prosvetov on 12.02.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
